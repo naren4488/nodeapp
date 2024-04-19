3cdc6518f21c41b4719dbdce30ad5090d37b22df
cloobot-narendra <narendra@cloobot.com>
2024-04-19 10:51:50 +0530

    third commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get("/api", (req, res) => {
   res.json({ message: "hello world" });
 });
 
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("listening at 8080");
 });
